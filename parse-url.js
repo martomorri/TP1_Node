e0bf3006c7075f80d632c9e23eb50dbc47bda7d0
martomorri <106319796+martomorri@users.noreply.github.com>
2023-04-14 11:56:56 -0300

    intento del ej6

--- parse-url.js
+++ parse-url.js
@@ -1,7 +1,9 @@
 import url from 'url';
 
+// Esto era el EJ5
 //export const parseUrl = a => url.parse(a, true);
 
+// Esto es el EJ6
 export const parseUrl = a => {
     try {
         return url.parse(a, true);
