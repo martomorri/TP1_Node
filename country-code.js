2f5142fbaf373abbc1309de7ef8f4b6758d09518
martomorri <106319796+martomorri@users.noreply.github.com>
2023-04-21 09:54:15 -0300

    Terminado

--- country-code.js
+++ country-code.js
@@ -1,6 +1,10 @@
 import countriesData from 'country-codes-list';
 
 export const obtenerMoneda = countryCode => {
-    const myCountryCodesObject = countriesData.customList('countryCode', 'La moneda de {countryNameEn} es {currencyCode}');
-    return myCountryCodesObject[countryCode];
+    const myCountryCodesObject = countriesData.customList('countryCode', '{currencyCode}');
+    if (myCountryCodesObject[countryCode]) {
+        return myCountryCodesObject[countryCode];
+    } else {
+        return null;
+    }
 }
